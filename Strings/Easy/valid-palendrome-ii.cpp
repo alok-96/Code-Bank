a784324cb5dadf9d62d2146d0795cbe3441cf839
alok96 <dwivedialok799@gmail.com>
2023-10-20 23:32:08 +0530

    20-oct-2023

--- Strings/Easy/valid-palendrome-ii.cpp
+++ Strings/Easy/valid-palendrome-ii.cpp
@@ -1,21 +1,44 @@
-class Solution {
-public:
-    bool validPalindrome(string s) {
-        int i=0, j = s.size()-1;
+// https://leetcode.com/problems/valid-palindrome-ii/
 
-        while(i < j)
+// T.C. - O(n), S.C. - O(1)
+// use two pointer if s[i] == s[j] then i++, j--. if not same then check for the substring removing one character from starting 
+// that is it palindrome or not and do the same after removing last character. if anyone is palindrome then return true.
+
+class Solution
+{
+public:
+    bool isPalindrome(string s, int i, int j)
+    {
+        while (i <= j)
         {
-            if(s[i] == s[j])
+            if (s[i] == s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s)
+    {
+        int i = 0, j = s.size() - 1;
+
+        while (i < j)
+        {
+            if (s[i] == s[j])
             {
                 i++;
                 j--;
             }
             else
             {
-                if(j-i == 1)
-                return true;
-                else
-                return false;
+                bool subStr1 = isPalindrome(s, i, j - 1);
+                bool subStr2 = isPalindrome(s, i + 1, j);
+
+                return subStr1 || subStr2;
             }
         }
         return true;
